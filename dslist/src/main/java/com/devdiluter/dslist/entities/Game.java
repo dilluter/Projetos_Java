d24a6af0ceb85f79f85e5fe73eada812f7d8f54d
igor bruno <igordf2502@gmail.com>
2025-05-20 09:31:36 -0300

    jAVASPRING2

--- dslist/src/main/java/com/devdiluter/dslist/entities/Game.java
+++ dslist/src/main/java/com/devdiluter/dslist/entities/Game.java
@@ -23,7 +23,9 @@ public class Game {
     private Integer year;
 
     private String genre;
-    private String platform;
+    private String platforms;
+    @Column
+    private Double score;
     private String imgUrl;
 
     @Column(columnDefinition = "TEXT")
@@ -35,13 +37,14 @@ public class Game {
     public Game() {
     }
 
-    public Game(Long id, String title, Integer year, String genre, String platform, String imgUrl,
+    public Game(Long id, String title, Integer year, String genre, String platforms,Double score ,String imgUrl,
                 String shortDescription, String longDescription) {
         this.id = id;
         this.title = title;
         this.year = year;
         this.genre = genre;
-        this.platform = platform;
+        this.platforms = platforms;
+        this.score = score;
         this.imgUrl = imgUrl;
         this.shortDescription = shortDescription;
         this.longDescription = longDescription;
@@ -79,13 +82,21 @@ public class Game {
         this.genre = genre;
     }
 
-    public String getPlatform() {
-        return platform;
+    public String getPlatforms() {
+        return platforms;
     }
 
-    public void setPlatform(String platform) {
-        this.platform = platform;
+    public void setPlatforms(String platforms) {
+        this.platforms = platforms;
     }
+    public Double getScore() {
+        return score;
+    }
+
+    public void setScore(Double score) {
+        this.score = score;
+    }
+
 
     public String getImgUrl() {
         return imgUrl;
