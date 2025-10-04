20f88243dab8cd17623332949d89a87d1eadf426
igor bruno <igordf2502@gmail.com>
2025-10-04 03:01:02 -0300

    retirando projeto antigo

--- dslist/src/main/java/com/devdiluter/dslist/repositories/GameListRepository.java
+++
@@ -1,9 +0,0 @@
-package com.devdiluter.dslist.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.devdiluter.dslist.entities.GameList;
-
-public interface GameListRepository extends JpaRepository<GameList, Long>{
-
-}
